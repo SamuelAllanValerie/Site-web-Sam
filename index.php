923a232a49d7c259e900a807ba6b6bcd1a2e2ae3
SamuelAllanValerie <47207675+SamuelAllanValerie@users.noreply.github.com>
2019-05-25 16:14:18 +0200

    Add files via upload
    Site Web de Sam.
    
    1 - suppression du footer et instauration d'un lien externe.

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Site de Samuel Valerie</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<section id="header">
+				<header class="major">
+					<h1>Bienvenue sur mon site!</h1>
+					<p>Grand passionné de r&eacute;f&eacute;rencement Web et de strat&eacute;gie digitale</a></p>
+				</header>
+				<div class="container">
+					<ul class="actions special">
+						<li><a href="#one" class="button primary scrolly">D&eacute;marrons</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic01.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Qui suis-je?</h2>
+						</header>
+						<p>Du haut de mes 33 ans, je me consid&egrave;re comme &eacute;tant un passion&eacute; du r&eacute;f&eacute;rencement naturel, en prime d’&ecirc;tre un fervent d&eacute;fenseur des contenus textuels. Que serait un site Web esth&eacute;tique sans mati&egrave;re ?</p>
+					</div>
+					<a href="#two" class="goto-next scrolly">Suivant</a>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic02.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Ce que je sais faire</h2>
+						</header>
+						<p>Mes atouts</p>
+						<ul class="icons-grid">
+							<li>
+								<span class="icon major fa-google"></span>
+								<h3>SEO/SEA</h3>
+							</li>
+							<li>
+								<span class="icon major fa-pencil"></span>
+								<h3>R&eacute;daction Web</h3>
+							</li>
+							<li>
+								<span class="icon major fa-code"></span>
+								<h3>HTML/CSS/PhP</h3>
+							</li>
+							<li>
+								<span class="icon major fa-facebook"></span>
+								<h3>Community Management</h3>
+							</li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic03.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Dernière petite chose</h2>
+						</header>
+						<p>Je r&eacute;side &agrave; &Eacute;vreux et je suis une personne qui aime relever des d&eacute;fis, notamment en mati&egrave;re de strat&eacute;gie digitale.<a href="http://localhost:7777/filrouge/"> Voir mes comp&eacute;tences.</a></p>
+					</div>
+					<a href="#footer" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+			<section id="footer">
+				<div class="container">
+					<h3><a href="http://localhost:7777/registration/register.php">Restons en contact</a><h3>
+				</div>
+				<footer>
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-linkedin"><span class="label">Linkedin</span></a></li>
+						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; 2019 Samuel Val&eacute;rie - Tous droits r&eacute;serv&eacute;s</li></a></li>
+					</ul>
+				</footer>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
